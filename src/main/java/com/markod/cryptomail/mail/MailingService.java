d9d9cd6268cb8e69ca438ee9a5daa2bd180afefd
Marko Dujović <dujovicm@outlook.com>
2023-01-13 14:35:24 +0100

    Messaging fixup

--- src/main/java/com/markod/cryptomail/mail/MailingService.java
+++ src/main/java/com/markod/cryptomail/mail/MailingService.java
@@ -3,9 +3,18 @@ package com.markod.cryptomail.mail;
 import com.markod.cryptomail.auth.IMAPAuthenticationService;
 import com.markod.cryptomail.auth.SMTPAuthenticationService;
 import com.markod.cryptomail.mail.models.FileModel;
+import com.markod.cryptomail.util.Encryption;
 import com.markod.cryptomail.util.IMail;
+import jakarta.activation.DataHandler;
 import jakarta.mail.*;
 import jakarta.mail.internet.InternetAddress;
+import javafx.util.Pair;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 public class MailingService implements IMail<MailingService> {
     private static MailingService INSTANCE;
@@ -24,6 +33,11 @@ public class MailingService implements IMail<MailingService> {
             throw new RuntimeException(e);
         }
 
+        return this;
+    }
+
+    public MailingService encrypt(String password, Encryption.PBEAlgorithm pbeAlgorithm, Encryption.DigestAlgorithm digestAlgorithm) {
+        mail.encrypt(password, pbeAlgorithm, digestAlgorithm);
         return this;
     }
 
@@ -91,5 +105,58 @@ public class MailingService implements IMail<MailingService> {
         }
 
         return INSTANCE;
+    }
+
+    public String getMessageContent(Message message) throws MessagingException, IOException {
+        String mailContent = "";
+        Object content = message.getContent();
+
+        if (content instanceof Multipart multipart) {
+            for (int i = 0; i < multipart.getCount(); i++) {
+                if (multipart.getBodyPart(i).getFileName() == null) {
+                    if (multipart.getBodyPart(i).isMimeType("text/plain") || multipart.getBodyPart(i).isMimeType("text/html")) {
+                        mailContent = (String) multipart.getBodyPart(i).getContent();
+                    }
+                }
+            }
+        }
+        else if (content instanceof String contentText) {
+            mailContent = contentText;
+        }
+
+        return mailContent;
+    }
+
+    public ArrayList<FileModel> getFileModels(Message message) throws MessagingException, IOException {
+        ArrayList<Pair<String, byte[]>> pairs = getMessageAttachments(message);
+        ArrayList<FileModel> fileModels = new ArrayList<>();
+
+        for (Pair<String, byte[]> pair : pairs) {
+            fileModels.add(new FileModel(pair.getKey(), pair.getValue()));
+        }
+
+        return fileModels;
+    }
+
+    private ArrayList<Pair<String, byte[]>> getMessageAttachments(Message message) throws MessagingException, IOException {
+        ArrayList<Pair<String, byte[]>> pairs = new ArrayList<>();
+        Object content = message.getContent();
+
+        if (content instanceof  Multipart multipart) {
+            for (int i = 0; i < multipart.getCount(); i++) {
+                if (multipart.getBodyPart(i).getContent() instanceof InputStream || multipart.getBodyPart(i).getContent() instanceof String) {
+                    if (Part.ATTACHMENT.equalsIgnoreCase(multipart.getBodyPart(i).getDisposition()) || multipart.getBodyPart(i).getFileName() != null) {
+                        DataHandler handler = multipart.getBodyPart(i).getDataHandler();
+                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                        handler.getInputStream().transferTo(byteArrayOutputStream);
+
+                        byte[] bytes = byteArrayOutputStream.toByteArray();
+
+                        pairs.add(new Pair<>(multipart.getBodyPart(i).getFileName(), bytes));
+                    }
+                }
+            }
+        }
+        return pairs;
     }
 }
--- src/main/java/com/markod/cryptomail/mail/models/MessageModel.java
+++ src/main/java/com/markod/cryptomail/mail/models/MessageModel.java
@@ -3,4 +3,6 @@ package com.markod.cryptomail.mail.models;
 
 import java.util.Date;
 
-public record MessageModel(int messageId, String messageSubject, String messageSender, Date messageDate) {}
+public record MessageModel(int messageId, String messageSubject, String messageSender, Date messageDate) {
+
+}
